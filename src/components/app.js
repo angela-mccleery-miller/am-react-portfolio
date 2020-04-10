a0a241b0edefe88410725d85fc27e35f144c295a
angela-mccleery-miller <angemam@gmail.com>
2020-04-09 18:01:42 -0600

    update to p-container & p-items, along with styling

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,6 @@ import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
-import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -14,13 +13,9 @@ import NoMatch from "./pages/no-match"
 
 
 export default class App extends Component {
- 
-  
-
-  render() {
+   render() {
     return (
       <div className="app">
-
         <Router>
 
           <div>
--- src/components/portfolio/portfolio-items.js
+++ src/components/portfolio/portfolio-items.js
@@ -9,7 +9,7 @@ export default function(props) {
 
   return (
     <div>
-        <h3>{props.title}</h3>
+        <h3>{props.name}</h3>
         <h4>{props.url}</h4>
         <Link to={`/portfolio/${props.slug}`}>Link</Link>
     </div>
