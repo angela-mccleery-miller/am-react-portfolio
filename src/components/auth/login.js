5ece82f934fd608050f31803e567afc1ac720511
angela-mccleery-miller <angemam@gmail.com>
2020-04-14 17:17:08 -0600

    update to Auth routes adding render props

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,66 +1,86 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            email: "",
-            password: ""
-        };
+  constructor(props) {
+    super(props);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      email: "",
+      password: "",
+      errorText: "",
+    };
 
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-   
-    handleChange(event) {
+    
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorText: ""
+    });
+  }
+
+  handleSubmit(event) {
+    axios
+      .post(
+        "https://api.devcamp.space/sessions",
+        {
+          client: {
+            email: this.state.email,
+            password: this.state.password
+          }
+        },
+            { withCredentials: true }
+      )
+      .then(response => {
+        if (response.data.status === 'created') {
+            console.log("You can come in...")
+        } else {
+            this.setState({
+                errorText: "Wrong email or password"
+            })
+        }      
+        }).catch(error => {
         this.setState({
-            [event.target.name]: event.target.value
-        });
+            errorText: "An error occurred"
+        })
+      });
 
-    }
+    event.preventDefault();
+  }
 
-    handleSubmit(event) {
-        console.log("Handle submit", event);
-    }
+  render() {
+    return (
+      <div>
+        <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
 
-    render() {
-        return (
+        <div>{this.state.errorText}</div>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
           <div>
-
-            <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
-{/* 
-            <h2>{this.state.name}</h2>
-            <h2>{this.state.password}</h2> */}
-
-            <form 
-            onSubmit={this.handleSubmit}>
-              <input 
-                type="email" 
-                name="email"
-                placeholder="Your email"
-                value={this.state.email}
-                onChange={this.handleChange}
-                
-              />
-
-              <input 
-                type="password"
-                name="password"
-                placeholder="Your password"
-                value={this.state.password}
-                onChange={this.handleChange}
-                
-            />
-
-             <div>
-                <button type="submit">Login</button>
-            </div>
-           </form>
+            <button type="submit">Login</button>
           </div>
-        );
-    }
+        </form>
+      </div>
+    );
+  }
 }
